b85c93c7fa9a7163aca185bf72bc16d36868662a
Amanda.Santos <amanda.santos@zup.com.br>
2023-09-06 11:47:40 -0300

    Upando aula 91

---
+++ app/src/main/java/br/com/douglasmotta/mysubscribers/data/db/dao/SubscriberDAO.kt
@@ -0,0 +1,23 @@
+package br.com.douglasmotta.mysubscribers.data.db.dao
+
+import androidx.room.*
+import br.com.douglasmotta.mysubscribers.data.db.entity.SubscriberEntity
+
+@Dao
+interface SubscriberDAO {
+
+    @Insert
+    suspend fun insert(subscriber: SubscriberEntity): Long
+
+    @Update
+    suspend fun update(subscriber: SubscriberEntity)
+
+    @Query("DELETE FROM subscriber WHERE id = :id")
+    suspend fun delete(id: Long)
+
+    @Query("DELETE FROM subscriber")
+    suspend fun deleteAll()
+
+    @Query("SELECT * FROM subscriber")
+    suspend fun getAll(): List<SubscriberEntity>
+}
---
+++ app/src/main/java/br/com/douglasmotta/mysubscribers/ui/subiscriberlist/SubscriberListFragment.kt
@@ -0,0 +1,82 @@
+package br.com.douglasmotta.mysubscribers.ui.subiscriberlist
+
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.observe
+import androidx.navigation.fragment.findNavController
+import br.com.douglasmotta.mysubscribers.R
+import br.com.douglasmotta.mysubscribers.data.db.AppDatabase
+import br.com.douglasmotta.mysubscribers.extension.navigateWithAnimations
+import br.com.douglasmotta.mysubscribers.repository.DatabaseDataSource
+import br.com.douglasmotta.mysubscribers.repository.SubscriberRepository
+import kotlinx.android.synthetic.main.subscriber_list_fragment.*
+
+class SubscriberListFragment : Fragment(R.layout.subscriber_list_fragment) {
+
+    private val viewModel: SubscriberListViewModel by viewModels {
+        object : ViewModelProvider.Factory {
+            override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+                val subscriberDao = AppDatabase.getInstance(requireContext()).subscriberDAO
+                val repository: SubscriberRepository = DatabaseDataSource(subscriberDao)
+                return SubscriberListViewModel(repository) as T
+            }
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        observeViewModelEvents()
+        configureViewListeners()
+    }
+
+    private fun observeViewModelEvents() {
+        viewModel.allSubscribersEvent.observe(viewLifecycleOwner) { allSubscribers ->
+            setHasOptionsMenu(allSubscribers.size > 1)
+
+            val subscriberListAdapter = SubscriberListAdapter(allSubscribers) { subscriber ->
+                val directions = SubscriberListFragmentDirections
+                    .actionSubscriberListFragmentToSubscriberFragment(subscriber)
+
+                findNavController().navigateWithAnimations(directions)
+            }
+
+            with(recycler_subscribers) {
+                setHasFixedSize(true)
+                adapter = subscriberListAdapter
+            }
+        }
+
+        viewModel.deleteAllSubscribersEvent.observe(viewLifecycleOwner) {
+            viewModel.getSubscribers()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        viewModel.getSubscribers()
+    }
+
+    private fun configureViewListeners() {
+        fabAddSubscriber.setOnClickListener {
+            findNavController().navigateWithAnimations(R.id.action_subscriberListFragment_to_subscriberFragment)
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.subscriber_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return if (item.itemId == R.id.delete_subscribers) {
+            viewModel.deleteAllSubscribers()
+            true
+        } else super.onOptionsItemSelected(item)
+    }
+}
